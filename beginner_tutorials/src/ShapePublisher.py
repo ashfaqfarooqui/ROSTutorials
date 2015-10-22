925f3bf42ee854e6cdc5dd8472d94dab695a87f5
Ashfaq Farooqui <ashfaq.farooqui@gmail.com>
2015-10-22 13:56:44 +0200

    base structure to start building simple applications in ROS

--- beginner_tutorials/src/ShapePublisher.py
+++ beginner_tutorials/src/ShapePublisher.py
@@ -2,31 +2,41 @@
 import roslib
 import rospy
 from geometry_msgs.msg import Twist
-
+from turtlesim.msg import Pose
+from tut_msgs.srv import *
 
 roslib.load_manifest('beginner_tutorials')
 rospy.init_node('ShapePublisher')
 
 
-
+#Create a publisher to publish on the correct topic
+#twistPublisher = rospy.Publisher("""add Topic name here""",Twist,queue_size = 100) 
 def main():
-    twistPublisher = rospy.Publisher('/turtle1/cmd_vel',Twist,queue_size = 100)
-    rate = rospy.Rate(1)
-    i = 0
+
     while not rospy.is_shutdown():
         rospy.loginfo("In while")
-        msg = Twist()
-        msg.linear.x = 0
-        msg.linear.y = 0
-        msg.linear.z = 0
+        #Add code here
+        rospy.spin() #This will block termination of the program. It can be replaced by rospy.sleep(s), to repeate the while loop every s seconds
 
-        msg.angular.x = i
-        msg.angular.y = 0
-        msg.angular.z = 0
-        rospy.loginfo(msg)
-        twistPublisher.publish(msg)
-        i= i+1
-        rate.sleep()
+ """ Use the method as a callback from the service"""
+#def handleShapeSelector(msg):
+#    return shapeSelectorResponse() 
+
+""" This method will command the turle to move. x specifies the 
+    linear movement and z specifies the rotational movement"""
+def cmdTurtle(x,z):
+    msg = Twist()
+    msg.linear.x = x
+    msg.linear.y = 0
+    msg.linear.z = 0
+
+    msg.angular.x = 0
+    msg.angular.y = 0
+    msg.angular.z = z
+    rospy.sleep(1)
+    #twistPublisher.publish(msg)
+
+
 
 if __name__ == '__main__':
     main()
